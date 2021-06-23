306258b7adeaf783557aafc9b90603d70f5a21bd
Riaz <almahmudr1@gmail.com>
2021-06-23 22:00:46 +0600

    M6F4 Done

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,13 @@ Route::group(['middleware'=>['sess']] , function(){
     Route::get('/system/sales/physical_store/sales_log','SaleController@sellLog');
     Route::get('/system/sales/physical_store/checkProductDetails','SaleController@productDetails');
 
+    //product manage
+    Route::get('/system/product_management','ProductController@index');
+    Route::get('/system/product_management/existing_products','ProductController@ExistingProduct');
+    Route::get('system/product_management/product/{product_id}', 'ProductController@ProductDetails');
+    Route::get('/system/product_management/existing_products/delete/{product_id}', 'ProductController@ProductDelete');
+    Route::get('/system/product_management/existing_products/edit/{product_id}', 'ProductController@ProductDetails');
+
 });
 
 Route::get('/logout','SigninController@logout');
