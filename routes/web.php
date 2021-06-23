5792e8cde8c2f97d741504ec2368eda33e4f6b32
Riaz <almahmudr1@gmail.com>
2021-06-23 12:42:23 +0600

    Registration Module (M3) Done

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/signup', function () {
+    return view('LoginSignUp.Registration')->with('title', 'Sign up');
+});
+
+Route::post('/signup','SignupController@signup');
+
+Route::get('/login', function () {
+    return view('LoginSignUp.Login')->with('title', 'Sign In');
+});
